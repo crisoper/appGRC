8989dd7393aee96eb3626af7cd6236f42abf7dc6
Crisoper <crisoper@ecis008.regioncajamarca.gob.pe>
2016-06-07 15:28:43 -0500

    Initial Commit

---
+++ AppGRC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppGRC
+//
+//  Created by Crisoper on 7/06/16.
+//  Copyright © 2016 Crisoper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
